a5a881291b1752ccf4a1ed7631bb287005650cec
tobyatgithub <toby.fangyuan@gmail.com>
2019-01-21 18:56:43 -0800

    added styling

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ cd /src
 # coverage run --source='.' manage.py test -v 1
 # coverage report
 python3 manage.py collectstatic --noinput
-# python3 manage.py makemigrations --noinput  # flask migrate
-# python3 manage.py migrate --noinput  # flask upgrade
+python3 manage.py makemigrations --noinput  # flask migrate
+python3 manage.py migrate --noinput  # flask upgrade
 python3 manage.py runserver 0.0.0.0:8000
